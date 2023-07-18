e20e3a564fc55fcfaca59b33926f93ca48324807
ONYXIY <onxvnezakona@gmail.com>
2023-07-18 23:43:43 +0300

    added full time update price

--- Angular-app/src/app/app.component.ts
+++ Angular-app/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { ICurrApiInterface } from './interface/interfaceForBtcApi';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Angular-app';
   currApi: ICurrApiInterface[] = [];
   selectSymbol: string = 'USD';
@@ -16,12 +16,16 @@ export class AppComponent {
 
 
   constructor(private http: HttpClient) {
+    setInterval(() =>{
     this.http.get<ICurrApiInterface[]>('https://blockchain.info/ticker').subscribe((res) =>{
     this.currApi = Object.values(res);
     console.log(this.currApi);
     this.filteredData = this.currApi;
     this.selectCoinBySymbol(this.selectSymbol)
     })
+  },2000)
+  }
+  ngOnInit(): void {
   }
   selectCoinBySymbol(symbol: string){
     this.selectSymbol = symbol;
