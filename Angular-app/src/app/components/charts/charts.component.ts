15da63297a45e472fe2c7aefc35be5d72fe19968
ONYXIY <onxvnezakona@gmail.com>
2023-07-21 04:41:39 +0300

    fix

--- Angular-app/src/app/components/charts/charts.component.ts
+++ Angular-app/src/app/components/charts/charts.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { ChartDataset, ChartOptions } from 'chart.js';
+import { ChartDataset, ChartOptions, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-charts',
@@ -8,12 +8,13 @@ import { ChartDataset, ChartOptions } from 'chart.js';
 })
 export class ChartsComponent {
 
+
   lineChartData: ChartDataset[] = [
     { 
       data: [85, 72, 78, 75, 77, 75], 
-      label: 'Crude oil prices',
-      backgroundColor: 'rgba(255, 255, 0, 0.28)', // Используем backgroundColor напрямую
-      borderColor: 'black' // Используем borderColor напрямую
+      label: 'BTC/USD',
+      backgroundColor: 'rgb(29, 45, 80);', 
+      borderColor: 'yellow'
     },
   ];
 
@@ -25,6 +26,7 @@ export class ChartsComponent {
 
   lineChartLegend = true;
   lineChartPlugins = [];
-  lineChartType = 'line';
+  lineChartType: ChartType = 'line';
+
 
 }
