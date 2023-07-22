4068a68fa7fe1b4e108ac57497d198149808be07
ONYXIY <onxvnezakona@gmail.com>
2023-07-22 18:31:00 +0300

    refac base on rxjs

--- Angular-app/src/app/components/base/base.component.ts
+++ Angular-app/src/app/components/base/base.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Observable, Subscription, interval, map, switchMap } from 'rxjs';
 import { ICurrApiInterface } from 'src/app/interface/interfaceForBtcApi';
 import { IBybitInterfacePrice } from 'src/app/interface/interfaceForBybitApi';
 
@@ -10,46 +11,45 @@ import { IBybitInterfacePrice } from 'src/app/interface/interfaceForBybitApi';
 })
 export class BaseComponent {
   currApiBlockchain: ICurrApiInterface[] = [];
-  selectSymbol: string = 'USD';
-  filteredData: ICurrApiInterface[] = [];
   blockchainApi: string = 'https://blockchain.info/ticker';
   bybitApi: string = 'https://api-testnet.bybit.com/v5/market/tickers?category=inverse&symbol=BTCUSD';
   currApiByBit: IBybitInterfacePrice[] = [];
   lastBitcoinPriceBybit!: number;
   lastBitcoinPriceBlockchain!: number;
   howMuchMorePrice: number = 0;
-  
+  private bybitApi$!: Observable<IBybitInterfacePrice[]>;
+  private bchApi$!: Observable<ICurrApiInterface[]>;
+
+  private intervalSubscription: Subscription | undefined;
  
 
 
   constructor(private http: HttpClient) {
-    setInterval(() =>{
-    this.http.get<ICurrApiInterface[]>(this.blockchainApi).subscribe((res) =>{
-    this.currApiBlockchain = Object.values(res);
-    this.lastBitcoinPriceBlockchain = this.currApiBlockchain[27].last;
-    console.log('Blockchain price: ' + this.lastBitcoinPriceBlockchain);
-    this.filteredData = this.currApiBlockchain;
-    this.selectCoinBySymbol(this.selectSymbol)
-    })
-  },5000);
-  setInterval(() =>{
-    this.http.get<IBybitInterfacePrice[]>(this.bybitApi).subscribe((res) =>{
-    this.currApiByBit = Object.values(res);
-    this.lastBitcoinPriceBybit = parseInt(this.currApiByBit[2].list[0].lastPrice)
-    console.log('Bybit price: ' + this.lastBitcoinPriceBybit);
-    
-    })
-   
-
-  },5000);
+  
   setInterval(()=>{
-    this.howMuchMore(this.lastBitcoinPriceBybit, this.lastBitcoinPriceBlockchain)}, 2000)
+    this.howMuchMore(this.lastBitcoinPriceBybit, this.lastBitcoinPriceBlockchain)}, 5000)
   }
+  
   ngOnInit(): void {
-  }
-  selectCoinBySymbol(symbol: string){
-    this.selectSymbol = symbol;
-    this.filteredData = this.currApiBlockchain.filter(item => item.symbol === symbol);
+    this.bchApi$ = interval(5000).pipe(
+      switchMap(() => this.http.get<ICurrApiInterface[]>(this.blockchainApi))
+    );
+  
+    this.bchApi$.subscribe((res: ICurrApiInterface[]) => {
+      this.currApiBlockchain = Object.values(res);
+      this.lastBitcoinPriceBlockchain = this.currApiBlockchain[27].last;
+      console.log('Blockchain price: ' + this.lastBitcoinPriceBlockchain);
+    });
+
+    this.bybitApi$ = interval(5000).pipe(
+      switchMap(() => this.http.get<IBybitInterfacePrice[]> (this.bybitApi))
+    );
+    this.bybitApi$.subscribe((res: IBybitInterfacePrice[]) =>{
+      this.currApiByBit = Object.values(res);
+      this.lastBitcoinPriceBybit = Number(this.currApiByBit[2].list[0].lastPrice)
+      console.log('Bybit price: ' + this.lastBitcoinPriceBybit)
+    })
+    
   }
   
   howMuchMore(lastBitcoinPriceBybit: number, lastBitcoinPriceBlockchain: number){
