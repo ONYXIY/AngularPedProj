8effef5135eabbace209209ceb618440e37ffbee
ONYXIY <onxvnezakona@gmail.com>
2023-07-23 11:40:45 +0300

    Added calc on bitcoin fee

--- Angular-app/src/app/components/base/base.component.ts
+++ Angular-app/src/app/components/base/base.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Observable, Subscription, interval, map, switchMap } from 'rxjs';
 import { ICurrApiInterface } from 'src/app/interface/interfaceForBtcApi';
 import { IBybitInterfacePrice } from 'src/app/interface/interfaceForBybitApi';
+import { IBTCFee } from 'src/app/interface/interfaceForTransferFee';
 
 @Component({
   selector: 'app-base',
@@ -14,16 +15,22 @@ export class BaseComponent {
   blockchainApi: string = 'https://blockchain.info/ticker';
   bybitApi: string = 'https://api-testnet.bybit.com/v5/market/tickers?category=inverse&symbol=BTCUSD';
   currApiByBit: IBybitInterfacePrice[] = [];
+  BTCFeeApi: string = 'https://api.blockchain.info/mempool/fees';
+  BTCFeePriority: IBTCFee[] = [];
+  BTCFeePriorityPrice!: IBTCFee | number;
   lastBitcoinPriceBybit!: number;
   lastBitcoinPriceBlockchain!: number;
   howMuchMorePrice: number = 0;
+  PriorityBTCFee!: number;
   private bybitApi$!: Observable<IBybitInterfacePrice[]>;
   private bchApi$!: Observable<ICurrApiInterface[]>;
+  private BtcFee$!: Observable<IBTCFee>;
+
 
   constructor(private http: HttpClient) {
   
   setInterval(()=>{
-    this.howMuchMore(this.lastBitcoinPriceBybit, this.lastBitcoinPriceBlockchain)}, 5000)
+    this.howMuchMore(this.lastBitcoinPriceBybit, this.lastBitcoinPriceBlockchain, Number(this.BTCFeePriorityPrice))}, 5000)
   }
   
   ngOnInit(): void {
@@ -44,18 +51,32 @@ export class BaseComponent {
       this.currApiByBit = Object.values(res);
       this.lastBitcoinPriceBybit = Number(this.currApiByBit[2].list[0].lastPrice)
       console.log('Bybit price: ' + this.lastBitcoinPriceBybit)
+    });
+    this.BtcFee$ = interval(5000).pipe(
+      switchMap(() => this.http.get<IBTCFee>(this.BTCFeeApi))
+    );
+    this.BtcFee$.subscribe((res: IBTCFee) =>{
+      this.BTCFeePriority = Object.values(res);
+      this.BTCFeePriorityPrice = this.BTCFeePriority[2]
+      console.log('BTC fee rn ' + this.BTCFeePriorityPrice)
     })
-    
+  
   }
   
-  howMuchMore(lastBitcoinPriceBybit: number, lastBitcoinPriceBlockchain: number){
+  howMuchMore(lastBitcoinPriceBybit: number, lastBitcoinPriceBlockchain: number, BTCFeePriorityPrice: number){
     if(lastBitcoinPriceBybit > lastBitcoinPriceBlockchain){
-      this.howMuchMorePrice = (lastBitcoinPriceBybit - lastBitcoinPriceBlockchain)
+      this.howMuchMorePrice = ((lastBitcoinPriceBybit - lastBitcoinPriceBlockchain) - BTCFeePriorityPrice)
        
     }else if(lastBitcoinPriceBybit < lastBitcoinPriceBlockchain){
-      this.howMuchMorePrice = (lastBitcoinPriceBlockchain - lastBitcoinPriceBybit)
+      this.howMuchMorePrice = ((lastBitcoinPriceBlockchain - lastBitcoinPriceBybit) - BTCFeePriorityPrice)
       
     }
       return this.howMuchMorePrice
+    }
+    AddBtcPrice(){
+      this.lastBitcoinPriceBlockchain = this.lastBitcoinPriceBlockchain + 1000 
+      console.log(this.lastBitcoinPriceBlockchain)
+      return this.lastBitcoinPriceBlockchain
+
     }
 }
---
+++ Angular-app/src/app/interface/interfaceForTransferFee.ts
@@ -0,0 +1,6 @@
+export interface IBTCFee {
+    limits: {
+        priority: number
+    }
+}
+
