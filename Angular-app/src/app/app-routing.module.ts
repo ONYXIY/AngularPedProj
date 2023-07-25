dcb724d44573b8c8912244e1ab24bf7d281fb270
ONYXIY <onxvnezakona@gmail.com>
2023-07-25 04:44:49 +0300

    add comp for reg/login

--- Angular-app/src/app/app-routing.module.ts
+++ Angular-app/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './components/profile/profile/profile.component';
+import { LoginComponent } from './components/login/login/login.component';
 
 const routes: Routes = [
   {path: '', loadChildren: () => import('./components/base/base.module').then(m =>m.BaseModule)},
+  {path: 'login', component: LoginComponent},
   {path: 'profile', component: ProfileComponent},
   {path: '**', redirectTo: '', component: AppComponent}
 ];
