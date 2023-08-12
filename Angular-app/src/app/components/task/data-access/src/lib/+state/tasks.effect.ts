e14254b04af1f91fc6ae5768210b647c8ca8631b
ONYXIY <onxvnezakona@gmail.com>
2023-08-12 21:34:07 +0300

    state for a new architecture

---
+++ Angular-app/src/app/components/task/data-access/src/lib/+state/tasks.effect.ts
@@ -0,0 +1,42 @@
+import { inject } from '@angular/core';
+import { Actions, createEffect, ofType } from "@ngrx/effects";
+import { ApiService } from '@users/core/http';
+import { tap, map, switchMap, mergeMap } from 'rxjs/operators'; 
+import { tasksAction } from './tasks.action';
+import { Store } from '@ngrx/store';
+import { ITaskBoard } from '../model/tasks.interface';
+
+export class tasksEffects {
+  
+  getColumn$ = createEffect(() => {
+    const actions$ = inject(Actions);
+    const api = inject(ApiService);
+    
+
+    return actions$.pipe(
+      ofType(tasksAction.getTasksColumn),
+      mergeMap(() =>
+        api.get<ITaskBoard>('/todos/me').pipe(
+          tap((res) => console.log('res', res)),
+          map((res) => tasksAction.getColumnSuccess(res))
+          ))
+    );
+  });
+
+//   postColumn$ = createEffect(() => {
+//     const actions$ = inject(Actions);
+//     const api = inject(ApiService);
+//     const store = inject(Store);
+
+//     return actions$.pipe(
+//       ofType(tasksAction.postChangeColumns),
+//       mergeMap(
+//         ({columns}) => api.post<void, ITaskBoard>(columns, '/todos/me' ).pipe(
+
+//         )
+
+//       )
+//     )
+// })
+
+  }
